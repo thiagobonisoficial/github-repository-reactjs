703a293ae6076b82b0927178046a90a9a2ee46f5
THIAGO DE BONIS <thiagodebonis@icloud.com>
2019-08-20 13:59:46 -0300

    Push Initial Configurations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    );
 }
